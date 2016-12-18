bccbb0f9aead3aa4d99a856399b18bbd8b9f7337
Angelo Cardellicchio <a.cardellicchio@gmail.com>
2016-12-18 20:16:00 +0100

    0.0.3
    [FEATURE] Added ImageReader

---
+++ libs/ExtractMultiShotGallery.m
@@ -0,0 +1,7 @@
+function [ output_args ] = ExtractMultiShotGallery( input_args )
+%EXTRACTMULTISHOTGALLERY Summary of this function goes here
+%   Detailed explanation goes here
+
+
+end
+
--- libs/ImageReader.m
+++ libs/ImageReader.m
@@ -1,22 +1,48 @@
-function dataset = ImageReader(dataset_name, dataset_folder, varargin)
-%IMAGEREADER Organize the images of a person re-identification dataset in a
-%tensor with a specific structure.
-%   DATASET = IMAGEREADER(DATASET_NAME, DATASET_FOLDER) reads each image in
-%   the DATASET_FOLDER, and store it in a H x W x K x N tensor, where H is
-%   the height of the image, W is the width of the image, K is the number
-%   of channels, and N is the cardinality of the DATASET_NAME dataset (i.e.
-%   the sum of the cardinality of both the gallery and the probe sets).
-%   
-%   DATASET = IMAGEREADER(DATASET_NAME, DATASET_FOLDER, NORMALIZE_IMGS, 
-%   HEIGHT, WIDTH) also, if NORMALIZE_IMGS is true, normalizes images to
-%   H = HEIGHT and W = WIDTH.
-%
-%   DATASET = IMAGEREADER(DATASET_NAME, DATASET_FOLDER, NEW_COLOR_SPACE)
-%   converts images in the color space specified by NEW_COLOR_SPACE.
-%   Current supported color spaces are RGB (default) and HSV.
+function [probe_dataset, gallery_dataset] = ImageReader(dataset_dir, ...
+                                            image_extension, pars)
+%IMAGEREADER Organize the images of a person re-identification dataset in
+%two tensors with a specific structure.
+%   DATASET = IMAGEREADER(DATASET_DIR, IMAGE_EXTENSION, PARS) reads the
+%   images with extension IMAGE_EXTENSION stored in the subfolders of 
+%   DATASET_DIR, which are assumed to be 'cam_a' and 'cam_b', and
+%   represents the probe and the gallery sets, respectively. Images are
+%   stored in a H x W x K x N tensor, where H is the height of the image, 
+%   W is the width of the image, K is the number of channels, and N is the 
+%   cardinality of both the probe and the gallery set. The values of H, W 
+%   and K are determined by the PARS structure.
 
-% TODO 1: CHECK
-
-
+    % Check inputs.
+    if nargin~=3
+        error('The number of input arguments is not correct.');
+    elseif ~isstruct(pars)
+        error('The parameters must be a structure.')
+    elseif ~((isfield(pars,'height'))...
+            &&(isfield(pars,'width'))...
+            &&(isfield(pars,'channels')))
+        error('Check pars structure')
+    elseif ~exist(dataset_dir,'dir')
+        error('Dataset Dir does not exist, or the path is not correct')
+    end
+    % Check if the dataset is multi-shot.
+    if(pars.multi_shot)
+        ExtractMultiShotGallery;
+    end
+    % Create structures for images.
+    probe_images = dir(fullfile(dataset_dir, 'cam_a', image_extension));
+    gallery_images = dir(fullfile(dataset_dir, 'cam_b', image_extension));
+    % Check if the probe set and the gallery set have the same number of
+    % images.
+    if length(probe_images)==length(gallery_images)
+        dataset_size = length(probe_images);
+    else
+        error('The probe set and the gallery set must have the same number of images');
+    end
+    % Create tensors.
+    probe_dataset = zeros(pars.height,pars.width,pars.channels,dataset_size);
+    gallery_dataset = zeros(pars.height,pars.width,pars.channels,dataset_size);
+    % Read images and store them in tensors.
+    parfor i = 1:dataset_size
+        probe_dataset(:,:,:,i) = imread(probe_images(i));
+        gallery_dataset(:,:,:,i) = imread(gallery_images(i));
+    end
 end
-
